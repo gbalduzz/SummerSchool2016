683a0fee32509a9d4c7ac362406c40daa3e51801
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-07-20 12:27:55 +0200

    ring is sone

--- topics/mpi/practicals/02.MPI_pt2pt/cxx/4.sum_ring.c
+++ topics/mpi/practicals/02.MPI_pt2pt/cxx/4.sum_ring.c
@@ -14,7 +14,8 @@
  ****************************************************************/
 
 
-#include <stdio.h>
+#include <iostream>
+#include <vector>
 #include <mpi.h>
 
 
@@ -23,7 +24,7 @@ int main (int argc, char *argv[])
     int my_rank, size;
     int snd_buf, rcv_buf;
     int right, left;
-    int sum, i;
+    int sum(0), i;
 
     MPI_Status  status;
     MPI_Request request;
@@ -36,17 +37,27 @@ int main (int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 
-    right = /* get rank of neighbor to your right */
-    left  = /* get rank of neighbor to your left */
+    right = (my_rank + 1) % size; /* get rank of neighbor to your right */
+    left  = (my_rank - 1 + size) % size; /* get rank of neighbor to your left */
 
+    std::vector<int> ranks(size);
     /* Implement ring addition code 
      * do not use if (rank == 0) .. else ..
      * every rank sends initialy its rank number to a neighbor, and then sends what
      * it receives from that neighbor, this is done n times with n = number of processes
      * all ranks will obtain the sum.
      */
+    ranks[0] = my_rank;
+    for(int i=1; i<size; i++){
+      MPI_Sendrecv(&ranks[i-1], 1, MPI_INT,
+                right, 0,
+                &ranks[i], 1, MPI_INT,
+                left, 0,
+		   MPI_COMM_WORLD, &status);
 
-    printf ("Process %i:\tSum = %i\n", my_rank, sum);
+    }
+    for(int i=0; i<ranks.size(); i++) sum += ranks[i];
+    std::cout<<"Process "<<my_rank<< "\tSum "<<sum<<"\n";
 
     MPI_Finalize();
     return 0;
